6752310f10a520b35ab235f505d9f02837fb2420
Thiago Navarro <dollynhoextremeyt@gmail.com>
2024-09-20 12:54:45 -0300

    alterações no botão de tema

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 function exibirMensagem() {
     document.getElementById('mensagem').style.display = 'block';
 }
+
 function voltarAoTopo() {
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
-const chk = document.getElementById('themeChk')
+
+const chk = document.getElementById('themeChk');
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('light')
-})
+    document.body.classList.toggle('light');
+});
