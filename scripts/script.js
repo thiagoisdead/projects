46905d34d4716c143a9155e9bbaff9f2d40ad52c
Thiago Navarro <dollynhoextremeyt@gmail.com>
2024-09-21 17:35:28 -0300

    adição do carrosel de imagens

--- scripts/script.js
+++ scripts/script.js
@@ -11,12 +11,14 @@ const chk = document.getElementById('themeChk');
 chk.addEventListener('change', () => {
     document.body.classList.toggle('light');
 });
+
 document.addEventListener('DOMContentLoaded', () => {
-    SVGInjector(document.querySelectorAll('.arrowInject'));
+    SVGInjector(document.querySelectorAll('.svgInject'));
 });
-window.addEventListener('scroll', function(){
-    let scroll = document.querySelector('.scrollTop')
-    scroll.classList.toggle('active', window.scrollY > 350)
-})
 
-
+window.addEventListener('scroll', function() {
+    let scroll = document.querySelector('.scrollTop');
+    scroll.classList.toggle('active', window.scrollY > 150);
+});
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
